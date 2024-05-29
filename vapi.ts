8e2618b7a99412e954407bd8ab6e0a58ec7a80ff
MacOS <hou.dobaotrung@gmail.com>
2024-05-29 16:07:48 +0700

    feat: update build

--- vapi.ts
+++ vapi.ts
@@ -22,12 +22,12 @@ async function startPlayer(player: HTMLAudioElement, track: any) {
     await player.play();
   }
 }
-async function buildAudioPlayer(track: any, participantId: string, config: VapiCallConfig) {
+async function buildAudioPlayer(track: any, participantId: string, config?: VapiCallConfig) {
   const player = document.createElement('audio');
   player.dataset.participantId = participantId;
   document.body.appendChild(player);
 
-  if (config.audioOutDeviceId) {
+  if (config?.audioOutDeviceId) {
     (player as any).sinkId = config.audioOutDeviceId;
   }
 
@@ -118,6 +118,7 @@ export default class Vapi extends VapiEventEmitter {
   constructor(apiToken: string, apiBaseUrl?: string) {
     super();
     client.baseUrl = apiBaseUrl ?? 'https://api.vapi.ai';
+    client.setSecurityData(apiToken);
   }
 
   private cleanup() {
